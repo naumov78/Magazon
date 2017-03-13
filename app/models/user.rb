5c053e9eecfef44c0096d9331ecfb0d9ee422c5f
Peter Tascio <peter.tascio@gmail.com>
2017-03-13 16:01:39 -0400

    Start user model

---
+++ app/models/user.rb
@@ -0,0 +1,29 @@
+class User < ActiveRecord:Base
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :password_digest, presence: true
+  validates :session_token, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+
+  attr_reader :password
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    user && user.is_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.gen_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+end
