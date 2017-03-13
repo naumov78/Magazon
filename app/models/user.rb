d3755aac835a15725fd01c6513900a3ec36f45cb
Peter Tascio <peter.tascio@gmail.com>
2017-03-13 16:06:52 -0400

    fix typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord:Base
     user && user.is_password?(password) ? user : nil
   end
 
-  def password=(password
+  def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
