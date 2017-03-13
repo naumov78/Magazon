da1ad5bce8433d9c10e4f7e3407760474b1a085f
Aleksei Naumov <naumov78@gmail.com>
2017-03-13 16:01:03 -0400

    add sessions_controller

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,28 @@
+class Api::SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+
+    if @user
+      login(@user)
+      render 'api/users/show'
+    else
+      render json: ['Wrong Emailor Password'], status: 401
+    end
+
+  end
+
+
+  def destroy
+    unless current_user.nil?
+      logout!
+      render json: {}
+    else
+      render json: ['No Current User'], status: 404
+    end
+  end
+
+
+end
