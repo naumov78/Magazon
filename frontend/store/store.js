3e1ba3521bd8e7ce75bfb585a9dab8fb0fe4714a
Peter Tascio <peter.tascio@gmail.com>
2017-03-20 11:34:09 -0400

    Finish store file

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,1 +1,18 @@
 import { createStore, applyMiddleware } from 'redux';
+import rootReducer from '../reducers/root_reducer';
+import thunkMiddleware from '../middleware/thunk';
+//may want to add createLogger for easier console reading
+
+const middleware = [];
+
+middleware.push(thunkMiddleware);
+
+const configureStore = (preloadedState = {}) => (
+  createStore(
+    rootReducer,
+    preloadedState,
+    applyMiddleware(...middleware)
+  )
+);
+
+export default configureStore;
