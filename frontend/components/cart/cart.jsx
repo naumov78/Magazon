dbe50c9c49939afa136eee51ff62756a1e900995
Aleksei Naumov <naumov78@gmail.com>
2017-03-25 19:28:49 -0400

    update cart component

--- frontend/components/cart/cart.jsx
+++ frontend/components/cart/cart.jsx
@@ -15,20 +15,52 @@ class Cart extends React.Component {
     const products = this.props.currentUser.cart
     return (
       <ul>
+        <li>
+          <table>
+            <tbody>
+              <tr>
+                <td>Product name</td>
+                <td>Description</td>
+                <td>Price</td>
+                <td>quantaty</td>
+                <td>Total</td>
+              </tr>
+            </tbody>
+          </table>
+        </li>
         {products.map((product, i) => {
           return (
             <li key={`${i}`}>
-              <div className="cart-product">
-              <Link to={`/categories/${product.category_id}/products/${product.id}`} >
-                This product: {product.title} is in your cart
-              </Link>
-              <p>------------</p>
-                {product.full_description}
-                {product.price}
-                  <div>
-                    <button onClick={(e) => this.removeFromCart(e, product.id)} >Remove from Cart</button>
-                  </div>
-                </div>
+              <table>
+                <tbody>
+                  <tr className="cart-product">
+                    <td>
+                      <Link to={`/categories/${product.category_id}/products/${product.id}`} >
+                        This product: {product.title} is in your cart
+                      </Link>
+                    </td>
+                    <td>
+                      {product.brief_description}
+                    </td>
+                    <td>
+                      {product.price}
+                    </td>
+                    <td>
+                      quantaty
+                    </td>
+                    <td>
+                      total
+                    </td>
+                  </tr>
+                  <tr>
+                    <td colSpan="4">
+                      <div>
+                        <button onClick={(e) => this.removeFromCart(e, product.id)} >Remove from Cart</button>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
             </li>
           )
         })}
