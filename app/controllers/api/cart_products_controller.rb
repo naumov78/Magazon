ce131a030ed28a92b5cc4b225c7f3837d9fc3b38
Aleksei Naumov <naumov78@gmail.com>
2017-03-28 10:22:56 -0400

    upd activrecord queries to avoid vulnerability for SQL injection

--- app/controllers/api/cart_products_controller.rb
+++ app/controllers/api/cart_products_controller.rb
@@ -3,12 +3,11 @@
 class Api::CartProductsController < ApplicationController
 
 def create
-  cart_product = current_user.cart.cart_products.where(product_id: cart_products_params[:product_id]).last
+  cart_product = current_user.cart.cart_products.where("product_id = ?", cart_products_params[:product_id]).first
   if cart_product.nil?
     cart_product = current_user.cart.cart_products.new(cart_products_params)
       if cart_product.save
-        @cart = CartProduct.all.where(cart_id: current_user.cart_id).order(created_at: :desc)
-        # @user = current_user
+        @cart = CartProduct.where(cart_id: current_user.cart_id).order(created_at: :desc)
         render 'api/carts/show'
       else
         render cart_product.errors.full_messages, status: 422
@@ -16,8 +15,7 @@ def create
   else
     cart_product.quantity += cart_products_params[:quantity].to_i
       if cart_product.save
-        @cart = CartProduct.all.where(cart_id: current_user.cart_id).order(created_at: :desc)
-        # @user = current_user
+        @cart = CartProduct.where(cart_id: current_user.cart_id).order(created_at: :desc)
         render 'api/carts/show'
       else
         render cart_product.errors.full_messages, status: 422
@@ -27,20 +25,18 @@ end
 
 
 def update
-  cart_product = current_user.cart.cart_products.where(product_id: cart_products_params[:product_id]).last
+  cart_product = current_user.cart.cart_products.where("product_id = ?", cart_products_params[:product_id]).first
   cart_product.quantity += cart_products_params[:quantity].to_i
   if cart_product.quantity < 1
     if cart_product.delete
-      @cart = CartProduct.all.where(cart_id: current_user.cart_id).order(created_at: :desc)
-      # @user = current_user
+      @cart = CartProduct.where(cart_id: current_user.cart_id).order(created_at: :desc)
       render 'api/carts/show'
     else
       render json: ['no such product in the cart'], status: 404
     end
   else
     if cart_product.save
-      @cart = CartProduct.all.where(cart_id: current_user.cart_id).order(created_at: :desc)
-      # @user = current_user
+      @cart = CartProduct.where(cart_id: current_user.cart_id).order(created_at: :desc)
       render 'api/carts/show'
     else
       render cart_product.errors.full_messages, status: 422
@@ -50,10 +46,9 @@ end
 
 
 def destroy
-  cart_product = CartProduct.where(cart_id: current_user.cart_id).where(product_id: cart_products_params[:product_id]).last
+  cart_product = CartProduct.where(cart_id: current_user.cart_id).where("product_id = ?", cart_products_params[:product_id]).first
   if cart_product.delete
-    @cart = CartProduct.all.where(cart_id: current_user.cart_id).order(created_at: :desc)
-    # @user = current_user
+    @cart = CartProduct.where(cart_id: current_user.cart_id).order(created_at: :desc)
     render 'api/carts/show'
   else
     render json: ['no such product in the cart'], status: 404
--- app/controllers/api/carts_controller.rb
+++ app/controllers/api/carts_controller.rb
@@ -1,55 +1,43 @@
 
 class Api::CartsController < ApplicationController
 
-  # def show
-  #   # @cart = Cart.find(current_user.cart_id)
-  #   # @product_quantity = @cart.products.reverse.each_with_object(Hash.new(0)) {|el, h| h[el] += 1 }
-  #   @product_quantity = Hash.new(0)
-  #   cart_products = CartProduct.all.where(cart_id: current_user.cart_id).order(created_at: :desc)
-  #   # debugger
-  #   cart_products.each do |pr|
-  #     product = Product.find(pr.product_id)
+  def show
+    # @cart = CartProduct.where(cart_id: current_user.cart_id).order(created_at: :desc)
+    @cart = current_user.cart.order(created_at: :desc)
+  end
+
+
+
+  # def create
+  #   @cart = Cart.find(current_user.cart_id)
+  #   @product_quantity = @cart.products.each_with_object(Hash.new(0)) {|el, h| h[el] += 1 }
+  #   cart_product = current_user.cart.cart_products.new(cart_params)
+  #   if cart_product.save
+  #     product = Product.find(cart_params[:product_id])
   #     @product_quantity[product] += 1
+  #   else
+  #     render cart_product.errors.full_messages, status: 422
   #   end
+  #   render '/api/carts/show'
+  # end
+  #
+  #
+  # def destroy
+  #   @cart = Cart.find(current_user.cart_id)
+  #   @product_quantity = @cart.products.each_with_object(Hash.new(0)) {|el, h| h[el] += 1 }
+  #   cart_product = CartProduct.where(cart_id: current_user.cart_id).where(product_id: cart_params[:product_id]).last
+  #   if cart_product.delete
+  #     product = Product.find(cart_params[:product_id])
+  #     @product_quantity[product] -= 1
+  #   else
+  #     render json: ['no such product in the cart'], status: 404
+  #   end
+  #   render '/api/carts/show'
   # end
 
-
-  def show
-    @cart = CartProduct.all.where(cart_id: current_user.cart_id).order(created_at: :desc)
-  end
-
-
-
-  def create
-    @cart = Cart.find(current_user.cart_id)
-    @product_quantity = @cart.products.each_with_object(Hash.new(0)) {|el, h| h[el] += 1 }
-    cart_product = current_user.cart.cart_products.new(cart_params)
-    if cart_product.save
-      product = Product.find(cart_params[:product_id])
-      @product_quantity[product] += 1
-    else
-      render cart_product.errors.full_messages, status: 422
-    end
-    render '/api/carts/show'
-  end
-
-
-  def destroy
-    @cart = Cart.find(current_user.cart_id)
-    @product_quantity = @cart.products.each_with_object(Hash.new(0)) {|el, h| h[el] += 1 }
-    cart_product = CartProduct.where(cart_id: current_user.cart_id).where(product_id: cart_params[:product_id]).last
-    if cart_product.delete
-      product = Product.find(cart_params[:product_id])
-      @product_quantity[product] -= 1
-    else
-      render json: ['no such product in the cart'], status: 404
-    end
-    render '/api/carts/show'
-  end
-
   def delete_all
-    CartProduct.delete_all
-    @product_quantity = Hash.new(0)
+    current_user.cart_products.delete_all
+    @cart = current_user.cart
     render '/api/carts/show'
   end
 
