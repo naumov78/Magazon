5f59891a703c6e0ec7dce1416ee21bf4ffd33439
Aleksei Naumov <naumov78@gmail.com>
2017-04-04 16:24:01 -0400

    add footer component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,19 +4,29 @@ import CategoriesContainer from './categories/categories_container';
 import SessionStatusContainer from './session_status/session_status_container';
 import StorefrontContainer from './storefront/storefront/storefront_container';
 import WatchedProductsContainer from './storefront/watched_products/watched_products_container';
+import Footer from './footer/footer';
 
 const App =({ children }) => {
   const a = children
     return(
-      <section>
+      <div id="top" className="main-container">
         <nav>
           <SessionStatusContainer />
           <CategoriesContainer />
         </nav>
+        <div>
           <StorefrontContainer />
+        </div>
+        <div>
           <WatchedProductsContainer />
+        </div>
+        <div>
           { children }
-      </section>
+        </div>
+        <div className="footer-container">
+          <Footer />
+        </div>
+      </div>
     );
   };
 
---
+++ frontend/components/footer/footer.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router';
+
+
+
+
+const Footer = (props) => {
+
+  function scrollToTop() {
+    window.scrollTo(0, 0)
+  }
+
+  debugger
+  return (
+    <div>
+      <a onClick={scrollToTop}>
+        <div className="back-2-top">
+          Back to top
+        </div>
+      </a>
+
+      <div className="footer-text">
+        <div className="magazon-description">
+            <div className="footer-titles">Project Description</div>
+            <div className="description-text">
+              Magazon is a full stack project based on Amazon.com.<br />
+              The content (product descriptions and product images) is the property of Amazon.com, Inc. and
+              used only for educational purposes.
+            </div>
+        </div>
+
+        <div className="magazon-authors">
+          <div className="footer-titles">Project Authors</div>
+          <div className="authors-text">
+            <span><a href="http://www.petertasc.io/" target="blank">Peter Tascio</a></span>
+              &nbsp;&nbsp;|&nbsp;&nbsp;
+            <span><a href="https://github.com/ptascio" target="blank">GitHub</a></span>
+            &nbsp;&nbsp;|&nbsp;&nbsp;
+            <span><a href="https://www.linkedin.com/in/peter-tascio-57a2245/" target="blank">LinkedIn</a></span>
+            <br />
+            <span><a href="http://alex-naumov.me/" target="blank">Aleksei Naumov</a></span>
+            &nbsp;&nbsp;|&nbsp;&nbsp;
+            <span><a href="https://github.com/naumov78" target="blank">GitHub</a></span>
+            &nbsp;&nbsp;|&nbsp;&nbsp;
+            <span><a href="https://www.linkedin.com/in/aleksei-naumov/" target="blank">LinkedIn</a></span><br />
+            <span><a href="https://github.com/naumov78/magazon" target="blank">Project GitHub repo</a></span>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  )
+}
+
+
+export default withRouter(Footer);
