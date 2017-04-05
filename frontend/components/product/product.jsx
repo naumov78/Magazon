20f5220331c6967bae9260a53382815a1d9eb04c
Aleksei Naumov <naumov78@gmail.com>
2017-04-05 15:53:50 -0400

    update product, storefront, footer styling

--- frontend/components/product/product.jsx
+++ frontend/components/product/product.jsx
@@ -15,11 +15,15 @@ class Product extends React.Component {
     this.props.getWatchedProducts()
   }
 
+  scrollToTop() {
+    window.scrollTo(0, 0)
+  }
+
   componentWillReceiveProps(nextProps) {
     debugger
     if (this.props.params !== nextProps.params) {
       this.props.fetchProduct(Number(nextProps.params.id[0]), Number(nextProps.params.id[1])).then((result) => {
-        this.setState({ product: result.product, main_picture: result.product.product_pictures[0] })
+        this.setState({ product: result.product, main_picture: result.product.product_pictures[0] }, this.scrollToTop())
       })
     }
   }
@@ -57,8 +61,8 @@ class Product extends React.Component {
     return (
       <div className="product-container">
 
-        <div className="alt-images">
-          <ul>
+        <div>
+          <ul className="alt-images">
             {this.state.product.product_pictures.map((picture, i) => {
               return (
                 <li key={i} className={this.getSmallImageClass(picture)}>
@@ -71,24 +75,30 @@ class Product extends React.Component {
           </ul>
         </div>
 
-        <div className="main-image">
-          <img width="522" height="522" src={this.state.main_picture.image_url} />
-        </div>
+        <div className="product-info-container">
 
-        <div className="product-info">
-
-          <div className="product-product-title">{this.state.product.title}</div>
-          <div className="product-product-descr">{this.state.product.full_description}</div>
-          <div className="product-product-price">
-            <span className="price-title">Price: </span>
-            <span className="price">${Number(this.state.product.price).toFixed(2)}</span>
-          </div>
-          <div className="product-order">
-            <span className="addToCart-button"><button onClick={() => this.addToCart(this.state.product.id, 1)}>Add to Cart</button></span>
+          <div className="main-image">
+            <img src={this.state.main_picture.image_url} />
           </div>
 
-          <div className="bought-together-container">
-            <BoughtTogether product={this.state.product} />
+          <div className="product-info">
+
+            <div className="product-product-title">{this.state.product.title}</div>
+            <div className="product-product-descr">{this.state.product.full_description}</div>
+            <div className="product-product-price">
+              <span className="price-title">Price: </span>
+              <span className="price">${Number(this.state.product.price).toFixed(2)}</span>
+            </div>
+            <div className="product-order">
+              <span className="addToCart-button"><button onClick={() => this.addToCart(this.state.product.id, 1)}>Add to Cart</button></span>
+            </div>
+
+
+            <div className="bought-together-container">
+              <BoughtTogether product={this.state.product} />
+            </div>
+
+
           </div>
 
         </div>
